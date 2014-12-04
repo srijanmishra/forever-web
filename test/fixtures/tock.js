947fafc2292d98ae9d46ccbf09173c3930cd821f
Tjatse <thisnamemeansnothing@gmail.com>
2014-12-04 11:20:37 +0800

    tock every 5 minutes

--- test/fixtures/tock.js
+++ test/fixtures/tock.js
@@ -3,4 +3,4 @@ var chalk = require('chalk');
 console.log(chalk.magenta('Tock every 5 seconds.'));
 setInterval(function(){
   console.log(chalk.bold.red.underline('Tock'), Date.now());
-}, 1000 - Date.now() % 1000);
+}, 5000);
