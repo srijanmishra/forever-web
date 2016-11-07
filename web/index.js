90a533ed7d3e3632c26050e464ee7e15599526ab
Srijan Misra <srijan23mishra@gmail.com>
2016-11-07 17:04:50 +0530

    Add authentication
    Adds basic authentication for access based on changes by:
    https://github.com/genecyber/forever-web
    
    Signed-off-by: Srijan Misra <srijan23mishra@gmail.com>

--- web/index.js
+++ web/index.js
@@ -9,11 +9,49 @@ module.exports = runServer;
 
 function runServer(port, debug) {
   var app = express();
-
+  var protected = false
   // all environments
   app.set('view engine', 'html');
   app.set('views', path.join(__dirname, 'views'));
   app.engine('html', swig.renderFile);
+
+  var mon = Monitor({
+    sockio: io,
+    debug: !!debug
+  });
+  if (mon._config.get("username") != undefined && mon._config.get("password") != undefined) { protected = true }
+  if (protected) {
+  app.use(function(req, res, next) {
+    var auth;
+    // check whether an autorization header was send
+    if (req.headers.authorization) {
+      // only accepting basic auth, so:
+      // * cut the starting "Basic " from the header
+      // * decode the base64 encoded username:password
+      // * split the string at the colon
+      // -> should result in an array
+      auth = new Buffer(req.headers.authorization.substring(6), 'base64').toString().split(':');
+    }
+
+    // checks if:
+    // * auth array exists
+    // * first value matches the expected user
+    // * second value the expected password
+    if (!auth || auth[0] !== mon._config.get('username') || auth[1] !== mon._config.get('password')) {
+        // any of the tests failed
+        // send an Basic Auth request (HTTP Code: 401 Unauthorized)
+        res.statusCode = 401;
+        // MyRealmName can be changed to anything, will be prompted to the user
+        res.setHeader('WWW-Authenticate', 'Basic realm="MyRealmName"');
+        // this will displayed in the browser when authorization is cancelled
+        res.end('Unauthorized');
+    } else {
+        // continue with processing, user was authenticated
+        next();
+    }
+ });
+}
+
   app.use(express.static(path.join(__dirname, 'public')));
 
   var log = Debug(({namespace: 'forever-web', debug: !!debug}));
@@ -29,7 +67,7 @@ function runServer(port, debug) {
   server.listen(port);
   log.i('http', 'Web server of', chalk.bold.underline('Nodejitsu/forever'), 'is listening on port', chalk.bold(port));
 
-  var mon = Monitor({
+   mon = Monitor({
     sockio: io,
     debug: !!debug
   });
